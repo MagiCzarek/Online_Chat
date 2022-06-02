63b4d0cea5098967ab07c657d3a912aef06e5e20
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2022-06-03 01:02:26 +0200

    upload secret key

--- Online_Chat/settings.py
+++ Online_Chat/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mvbesr0tmoak%z&-0@ueyy=y)70ot$!hfn&y@%m6*z(=mh1h6g'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
